bc640a96e88abd4d9e95f4e44d39e7da56950bec
Anzar Malik <anzarmalikcse@gmail.com>
2022-06-19 15:48:30 +0530

    added  basic get  API

--- youtube-url-service/api/candidate.js
+++ youtube-url-service/api/candidate.js
@@ -94,3 +94,27 @@ module.exports.list = (event, context, callback) => {
   dynamoDb.scan(params, onScan);
 
 };
+
+
+module.exports.get = (event, context, callback) => {
+  const params = {
+    TableName: process.env.CANDIDATE_TABLE,
+    Key: {
+      id: event.pathParameters.id,
+    },
+  };
+
+  dynamoDb.get(params).promise()
+    .then(result => {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(result.Item),
+      };
+      callback(null, response);
+    })
+    .catch(error => {
+      console.error(error);
+      callback(new Error('Couldn\'t fetch candidate.'));
+      return;
+    });
+};
